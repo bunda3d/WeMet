1cf48929f152b6aae8733b794bdb0c0894846540
Kris Bunda <14361990+bunda3d@users.noreply.github.com>
2020-01-26 20:22:35 -0600

    Installed Angular front end

--- wemet.API/Controllers/ValuesController.cs
+++ wemet.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wemet.API.Data;
 
 namespace wemet.API.Controllers
 {
@@ -10,18 +12,26 @@ namespace wemet.API.Controllers
   [ApiController]
   public class ValuesController : ControllerBase
   {
+    private readonly DataContext _context;
+    public ValuesController(DataContext context)
+    {
+      _context = context;
+    }
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public async Task<IActionResult> GetValues()
     {
-      return new string[] { "value1", "value2" };
+      var values = await _context.Values.ToListAsync();
+      return Ok(values);
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public async Task<IActionResult> GetValue(int id)
     {
-      return "value";
+      var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+
+      return Ok(value);
     }
 
     // POST api/values
