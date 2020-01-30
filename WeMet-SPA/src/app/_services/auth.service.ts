3909be83c07d4c89dbcef75bdfcc711548f81fbc
Kris Bunda <14361990+bunda3d@users.noreply.github.com>
2020-01-30 00:09:27 -0600

    added auth to navbar ng form

---
+++ WeMet-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+  constructor(private http: HttpClient) {}
+    // take json web token response (observable) as key/value pair object(tokenKey:"tokenValue") and convert to rjsx operator via .pipe():
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
+}
--- WeMet-SPA/src/app/nav/nav.component.ts
+++ WeMet-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  // obj to accept form inputs
+  model: any = {};
 
   title = 'WeMet!';
-  
-  constructor() { }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  // method to login input passed as model
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
   }
 
 }
