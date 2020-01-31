5f16136b39bbad3302fd4b3291a0319b894e75a6
Kris Bunda <14361990+bunda3d@users.noreply.github.com>
2020-01-30 22:33:50 -0600

    added registration, ng-bootstrap nav toggle

--- WeMet-SPA/src/app/app.module.ts
+++ WeMet-SPA/src/app/app.module.ts
@@ -7,23 +7,27 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-  AppComponent,
-  ValueComponent,
-  NavComponent
-  ],
-  imports: [
-  BrowserModule,
-  HttpClientModule,
-  FormsModule
-  ],
-  providers: [
-    AuthService
-  ],
-  bootstrap: [
-  AppComponent
-  ]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
